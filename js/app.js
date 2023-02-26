4092c967e87a04d168d218575e6972ff91971ac8
avillaq <villafuertequispealex@gmail.com>
2023-02-25 23:49:46 -0500

    boton search y Copy link terminado

--- js/app.js
+++ js/app.js
@@ -1,49 +1,47 @@
 document.addEventListener('DOMContentLoaded', init);
 function init() {
-    /*const hitomi = document.querySelector(".hitomi")
-    const nhentai = document.querySelector(".nhentai")
-    const boton1 = document.getElementById('boton1');
-
-    function buscar() {
-        if (hitomi.value.length === 5 || hitomi.value.length === 7) {
-            var win = window.open("https://hitomi.la/reader/" + hitomi.value + ".html#1", '_blank');
-            win.focus();
-
-        } else if (nhentai.value.length == 5 || nhentai.value.length == 6) {
-            var win = window.open('https://nhentai.to/g/' + nhentai.value, '_blank');
-            win.focus();
-        }
-    }
-    boton1.addEventListener('click', buscar);
-
-    //Muestra la url presionando el boton link
-    const boton2 = document.getElementById('boton2');
-    function mostrarLink() {
-        if (hitomi.value.length === 5 || hitomi.value.length === 7) {
-            //Con document.getElementById("id")."Atributo" podemos cambiar atrubutos del elemento con el ID proporcionado 
-            document.getElementById('campoLink').href = "https://hitomi.la/reader/" + hitomi.value + ".html#1";
-            document.getElementById('campoLink').innerHTML = "https://hitomi.la/reader/" + hitomi.value + ".html#1";
-
-        } else if (nhentai.value.length == 5 || nhentai.value.length == 6) {
-            document.getElementById('campoLink').href = 'https://nhentai.to/g/' + nhentai.value;
-            document.getElementById('campoLink').innerHTML = 'https://nhentai.to/g/' + nhentai.value;
-        }
-    }
-    boton2.addEventListener('click', mostrarLink);*/
-
-
-    //Habilita y deshabilita campos de texto
-    const logoHitomi = document.querySelector("#container-logo-hitomi");
-    const logoNhentai = document.querySelector("#container-logo-nhentai");
-
+    /** inputs */
     const inputHitomi = document.querySelector("#input-hitomi");
     const inputNhentai = document.querySelector("#input-nhentai");
 
+
+    const boton1 = document.getElementById('boton1');
+    boton1.addEventListener('click', function (e) {
+        e.preventDefault();
+        if (inputHitomi.value.length === 5 || inputHitomi.value.length === 7) {
+            let win = window.open("https://hitomi.la/reader/" + inputHitomi.value + ".html#1", '_blank');
+            win.focus();
+
+        } else if (inputNhentai.value.length == 5 || inputNhentai.value.length == 6) {
+            let win = window.open('https://nhentai.to/g/' + inputNhentai.value, '_blank');
+            win.focus();
+        }
+    });
+
+    
+    //Copia al portapapeles la url presionando el Copy Link
+    const boton2 = document.getElementById('boton2');
+    boton2.addEventListener('click', function(e){
+        e.preventDefault();
+        if (inputHitomi.value.length === 5 || inputHitomi.value.length === 7) {
+            navigator.clipboard.writeText("https://hitomi.la/reader/" + inputHitomi.value + ".html#1");
+        } else if (inputNhentai.value.length == 5 || inputNhentai.value.length == 6) {
+            navigator.clipboard.writeText('https://nhentai.to/g/' + inputNhentai.value);
+        }
+    });
+
+
+    /* Logos */
+    const logoHitomi = document.querySelector("#container-logo-hitomi");
+    const logoNhentai = document.querySelector("#container-logo-nhentai");
+
+    /**Eventos para los logos */
     logoHitomi.addEventListener('click', function (){
         slide("#container-hitomi","#container-nhentai");
         inputHitomi.disabled = false;
         inputNhentai.disabled = true;
     });
+    /**Eventos para los logos */
     logoNhentai.addEventListener('click', function (){
         slide("#container-nhentai","#container-hitomi");
         inputHitomi.disabled = true;
